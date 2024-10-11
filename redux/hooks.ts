838e84fc18d10b4c8b1607e231a5f07f51c398b1
ColinAdo <colinadodev30@gmail.com>
2024-10-11 22:37:43 +0300

    Removed unwanted imports

--- redux/hooks.ts
+++ redux/hooks.ts
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import type { TypedUseSelectorHook, UseDispatch } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
