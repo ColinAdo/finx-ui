5b79aeb8e2b3614d4b4847ce3bde1883872a2b90
ColinAdo <colinadodev30@gmail.com>
2024-10-12 22:24:57 +0300

    Removed unnecessary imports

--- app/dashboard/(messages)/messages/page.tsx
+++ app/dashboard/(messages)/messages/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import { UserAvatar } from "@/components/common";
-import { ProfileForm } from "@/components/profile";
 import { useRetrieveProfileQuery } from "@/redux/features/profileSlice";
-import { EllipsisVertical } from "lucide-react";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { Messages } from "@/components/profile";
 
 export default function EditProfile() {
