dd3c077ab9560cdf7b344ac6ebdd71af90d85f7f
ColinAdo <colinadodev30@gmail.com>
2024-10-22 22:17:01 +0300

    Add inbox route

---
+++ app/dashboard/(messages)/inbox/[id]/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRetrieveProfileQuery } from "@/redux/features/profileSlice";
+import { Messages } from "@/components/profile";
+
+export default function Page() {
+  const { data: profile } = useRetrieveProfileQuery();
+
+  if (!profile) {
+    return;
+  }
+
+  return (
+    <div className="px-12 h-screen lg:w-full">
+      <Messages recipientProfile={profile} />
+    </div>
+  );
+}
