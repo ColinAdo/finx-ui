db3e96deefcd17b9b6e523f5d219d35c291ae26e
ColinAdo <colinadodev30@gmail.com>
2024-10-23 09:14:56 +0300

    Add recipient profile

--- app/dashboard/(messages)/inbox/[id]/page.tsx
+++ app/dashboard/(messages)/inbox/[id]/page.tsx
@@ -1,18 +1,37 @@
 "use client";
 
-import { useRetrieveProfileQuery } from "@/redux/features/profileSlice";
+import { useRetrieveConversationQuery } from "@/redux/features/chatSlice";
 import { Messages } from "@/components/profile";
+import {
+  useRetrieveProfileQuery,
+  useRetrieveUsersProfileQuery,
+} from "@/redux/features/profileSlice";
 
-export default function Page() {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function Page({ params }: Props) {
   const { data: profile } = useRetrieveProfileQuery();
+  const { data: conversation } = useRetrieveConversationQuery(params.id);
 
-  if (!profile) {
+  const otherUser: any = conversation?.conversation.users.find(
+    (user) => user.id !== profile?.profile.id
+  );
+
+  const { data: userProfile } = useRetrieveUsersProfileQuery(
+    otherUser?.username
+  );
+
+  if (!userProfile) {
     return;
   }
 
   return (
     <div className="px-12 h-screen lg:w-full">
-      <Messages recipientProfile={profile} />
+      <Messages recipientProfile={userProfile} />
     </div>
   );
 }
