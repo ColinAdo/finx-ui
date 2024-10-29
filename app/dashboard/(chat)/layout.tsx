b8349c80f2fced74204e87a7c3d8a25e1e1e0fb3
ColinAdo <colinadodev30@gmail.com>
2024-10-29 08:13:03 +0300

    Add conversation list

--- app/dashboard/(chat)/layout.tsx
+++ app/dashboard/(chat)/layout.tsx
@@ -4,30 +4,10 @@ import { UserAvatar } from "@/components/common";
 import { buttonVariants } from "@/components/ui/button";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useRetrieveProfileQuery } from "@/redux/features/profileSlice";
+import { useGetConversationsQuery } from "@/redux/features/chatSlice";
 import { useAppSelector } from "@/redux/hooks";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-
-const tabs = [
-  { title: "Colin", value: "edit-profile", href: `messages` },
-  {
-    title: "Professional account",
-    value: "professional-account",
-    href: "/",
-  },
-  { title: "Notifications", value: "notifications", href: "/" },
-  {
-    title: "Privacy and security",
-    value: "privacy-and-security",
-    href: "/",
-  },
-  { title: "Login activity", value: "login-activity", href: "/" },
-  {
-    title: "Emails from Instagram",
-    value: "emails-from-instagram",
-    href: "/",
-  },
-];
 
 export default function SettingsLayout({
   children,
@@ -35,11 +15,13 @@ export default function SettingsLayout({
   children: React.ReactNode;
 }) {
   const { data: profile } = useRetrieveProfileQuery();
+  const { data: conversations } = useGetConversationsQuery();
   const profileData = useAppSelector((state) => state.auth.profilePicture);
 
-  if (!profile) {
+  if (!profile || !conversations) {
     return;
   }
+
   return (
     <div className="flex">
       <Tabs
@@ -48,25 +30,39 @@ export default function SettingsLayout({
         orientation="vertical"
       >
         <div className="flex fixed justify-between mx-4 lg:-ml-8 ml-12 lg:-mt-14 -mt-20">
-          <UserAvatar
-            user={profileData ? profileData : profile.profile}
-            className="h-10 w-10"
-          />
+          <Link href={`/dashboard/${profile.profile.username}`}>
+            <UserAvatar
+              user={profileData ? profileData : profile.profile}
+              className="h-10 w-10 hidden lg:block"
+            />
+          </Link>
           <h4 className="font-extrabold text-xl text-white ml-5">Chats</h4>
         </div>
         <TabsList className="flex flex-col items-start lg:-ml-12  ml-8 justify-start h-full bg-transparent">
-          {tabs.map((tab) => (
-            <TabsTrigger
-              key={tab.value}
-              value={tab.value}
-              className={cn(
-                buttonVariants({ variant: "ghost", size: "lg" }),
-                "data-[state=active]:bg-zinc-100 dark:data-[state=active]:bg-neutral-800 dark:hover:bg-neutral-900 w-full justify-start !px-3"
-              )}
-            >
-              <Link href={`${tab.href}`}>{tab.title}</Link>
-            </TabsTrigger>
-          ))}
+          {conversations.map((conversation) => {
+            const otherUser = conversation.users.find(
+              (user) => user.id !== profile.profile.id
+            );
+
+            return (
+              <TabsTrigger
+                key={conversation.id}
+                value={conversation.id}
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "lg" }),
+                  "data-[state=active]:bg-zinc-100 dark:data-[state=active]:bg-neutral-800 dark:hover:bg-neutral-900 w-full justify-start !px-3"
+                )}
+              >
+                {otherUser ? (
+                  <Link href={`/dashboard/inbox/${conversation.id}`}>
+                    {otherUser.username}
+                  </Link>
+                ) : (
+                  "No other user"
+                )}
+              </TabsTrigger>
+            );
+          })}
         </TabsList>
       </Tabs>
 
