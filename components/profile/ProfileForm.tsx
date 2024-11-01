3984abe2fe485854ae2cea58092daf253ffbc94d
ColinAdo <colinadodev30@gmail.com>
2024-11-01 19:55:46 +0300

    Add arranged import

--- components/profile/ProfileForm.tsx
+++ components/profile/ProfileForm.tsx
@@ -1,23 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -33,6 +17,22 @@ import {
   useRetrieveProfileQuery,
   useUpdateProfileMutation,
 } from "@/redux/features/profileSlice";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 interface Props {
   profile: ProfileData;
