9a92c6fa9cfd884f9c906c2563ee1f9e46519e9d
ColinAdo <colinadodev30@gmail.com>
2024-11-05 06:56:14 +0300

    Removed console log

--- hooks/use-register.ts
+++ hooks/use-register.ts
@@ -32,7 +32,6 @@ export default function useRegister() {
       })
       .catch((err) => {
         toast.error("Registration failed!");
-        console.log("Registration failed error", err);
       });
   };
   return {
